5907e63f01428a2072234dacd2148097702dd470
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-22 07:54:31 -0400

    added class definitions

--- Knapsack_solver.h
+++ Knapsack_solver.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "pch.h"
 /*
 File Name: Knapsack_solver.h
 Author: Evan Gertis.
@@ -6,13 +7,37 @@ Date: 03/22/2019.
 Purpose: Holds the class definitions for Knapsack_solver implementation.
 */
 
+/*BEGIN namespace*/
 namespace KN {
 
+	/*BEGIN class*/
 	class Knapsack_solver
 	{
+		//members.
+		int mNumOfItems;
+		int mCapcityOfKnapsack;
+		std::vector<std::vector<int>> mKnapsackTable;
+		int totalBenefit = 0;
+		std::vector<int> mWeights;
+		std::vector<int> mValues;
+
+	/**************************************/
+	/*************************************/
+	/*************************************/
 	public:
 		Knapsack_solver();
+		void operator()();
+		void showResult();
+
 		~Knapsack_solver();
+
+	/**************************************/
+	/*************************************/
+	/*************************************/
+	private:
+
 	};
+	/*END class*/
 
 }
+/*END namespace*/
